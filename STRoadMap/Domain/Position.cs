11173a1bcac530bde32fcaa10a404eb8b32ace2e
Albarn <elrib7vj@gmail.com>
2018-04-16 13:24:17 +0300

    add auto increment to data base and position level column to position table

--- STRoadMap/Domain/Position.cs
+++ STRoadMap/Domain/Position.cs
@@ -23,6 +23,7 @@ namespace Domain
         public string Name { get; set; }
         public int PositionId { get; set; }
         public int SpecializationId { get; set; }
+        public int PositionLevel { get; set; }
     
         public virtual Specialization Specialization { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- STRoadMap/Domain/RMDbModel.Designer.cs
+++ STRoadMap/Domain/RMDbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\elrib\source\repos\sigma\ST_RoadMap\STRoadMap\Domain\RMDbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\elrib\source\repos\ST_RoadMap\STRoadMap\Domain\RMDbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
