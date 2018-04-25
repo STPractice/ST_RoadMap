c180c852e631fbe2fb4c06f22a7fbb54196d52c6
bkrjbkrj <bkrj222@gmail.com>
2018-04-25 23:55:17 +0300

    A LOOOOT of bugs fixed

--- STRoadMap/STRoadMap/Scripts/HR/CreateSpecialization/HRCreateSpecialization.js
+++ STRoadMap/STRoadMap/Scripts/HR/CreateSpecialization/HRCreateSpecialization.js
@@ -4,15 +4,19 @@ var i = 0;
 $(document).ready(function () {
     $(document).on('click', '.add', function () {
         var addedSkill = document.getElementById(this.id.substring(3, this.id.length));
-        var elem = this.id.substring(3, this.id.length);
-        var text = addedSkill.firstElementChild.textContent;
-        $('#' + elem).remove();
+        var name = addedSkill.childNodes[1].textContent;
+        var id = addedSkill.childNodes[2].textContent;
+
+        var element = this.id.substring(3, this.id.length);       
+        $('#' + element).remove();
+
         $('.chosen').append($('<div></div>')
             .addClass('skill')
             .attr('id', i)
             .html(`
-           <p>${text}</p>
+           <p>${name}</p><p class="small">${id}</p>
            <div class="delete" id="delete${i}">Delete</div>
+           <input type="hidden" name="Skills[${i}].SkillId" value="${id}" />
         `)
         );
         i++;
@@ -20,15 +24,17 @@ $(document).ready(function () {
 
     $(document).on('click', '.delete', function () {        
         var deletedSkill = document.getElementById(this.id.substring(6, this.id.length));
-        var text = deletedSkill.firstElementChild.textContent;
-        var elem = this.id.substring(6, this.id.length);
-        $('#' + elem).remove();
+        var name = deletedSkill.childNodes[1].textContent;
+        var id = deletedSkill.childNodes[2].textContent;
+
+        var element = this.id.substring(6, this.id.length);
+        $('#' + element).remove();
 
         $('.available').append($('<div></div>')
             .addClass('skill')
             .attr('id', i + 100)
             .html(`
-           <p>${text}</p>
+           <p>${name}</p><p class="small">${id}</p>
            <div class="add" id="add${i + 100}">Add</div>
         `)
         );
