52d8795304992db2a5d2e43a2b795ec92777f969
Albarn <elrib7vj@gmail.com>
2018-05-01 11:37:31 +0300

    add view

--- STRoadMap/Domain/Data_Access/Implementation/EntitySkillMatrixRepository.cs
+++ STRoadMap/Domain/Data_Access/Implementation/EntitySkillMatrixRepository.cs
@@ -9,10 +9,12 @@ namespace Domain
     public class EntitySkillMatrixRepository : ISkillMatrixRepository
     {
         private readonly Entities1 context;
+        private readonly IUnitOfWork UoW;
 
-        public EntitySkillMatrixRepository(Entities1 context)
+        public EntitySkillMatrixRepository(Entities1 context, IUnitOfWork UoW)
         {
             this.context = context;
+            this.UoW = UoW;
         }
 
         public int Create(SkillMatrix entity)
@@ -42,8 +44,68 @@ namespace Domain
 
         public int Update(SkillMatrix entity)
         {
-            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
-            return entity.SkillMatrixId;
+            List<SkillLevel> innerSkillLevels = new List<SkillLevel>();
+            foreach (SkillLevel foreignSkillLevel in entity.SkillLevels)
+            {
+                SkillLevel innerSkillLevel = UoW.SkillLevels.Find(foreignSkillLevel.SkillLevelId);
+                if (innerSkillLevel != null)
+                {
+                    innerSkillLevels.Add(innerSkillLevel);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            entity.SkillLevels = innerSkillLevels;
+
+            SkillMatrix currentMatrix = Find(entity.SkillMatrixId);
+            if (currentMatrix == null) return 0;
+            currentMatrix.SpecializationId = entity.SpecializationId;
+            currentMatrix.EmpolyeeId = entity.EmpolyeeId;
+
+            List<SkillLevel> OldSkillLevels = currentMatrix.SkillLevels.ToList<SkillLevel>();
+            List<SkillLevel> NewSkillLevels = entity.SkillLevels.ToList<SkillLevel>();
+
+            foreach (SkillLevel oldSkillLevel in OldSkillLevels)
+            {
+                bool exist = false;
+
+                foreach (SkillLevel newSkillLevel in NewSkillLevels)
+                {
+                    if (oldSkillLevel.SkillLevelId == newSkillLevel.SkillLevelId)
+                    {
+                        exist = true;
+                    }
+                }
+                if (exist == false)
+                {
+                    currentMatrix.SkillLevels.Remove(oldSkillLevel);
+                }
+            }
+            foreach (SkillLevel newSkillLevel in NewSkillLevels)
+            {
+                bool exist = false;
+                foreach (SkillLevel oldSkillLevel in OldSkillLevels)
+                {
+                    if (newSkillLevel.SkillLevelId == oldSkillLevel.SkillLevelId) exist = true;
+                }
+                if (exist == false)
+                {
+                    currentMatrix.SkillLevels.Add(newSkillLevel);
+                }
+            }
+
+            try
+            {
+                context.Entry(currentMatrix).State = System.Data.Entity.EntityState.Modified;
+                return currentMatrix.SpecializationId;
+            }
+
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 }
--- STRoadMap/STRoadMap/Controllers/EmployeeController.cs
+++ STRoadMap/STRoadMap/Controllers/EmployeeController.cs
@@ -3,22 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Domain;
 using Extensibility;
 
 namespace STRoadMap.Controllers
 {
     public class EmployeeController : Controller
     {
-        private readonly IEmployeeLogic EmployeeLogic;
+        private readonly IEmployeeLogic employeeLogic;
 
         public EmployeeController(IEmployeeLogic EmployeeLogic)
         {
-            this.EmployeeLogic = EmployeeLogic;
+            this.employeeLogic = EmployeeLogic;
         }
         // GET: Employee
         public string Index()
         {
             return "It works)";
+        }
+
+        [HttpGet]
+        public ActionResult PerformanceReview(int? SpecializationId = 103)
+        {
+            if (SpecializationId == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Specialization spec = employeeLogic.GetSpecialization((int)SpecializationId);
+                if (spec == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+
+                    return View(spec);
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult PerformanceReview(SkillMatrix position)
+        {
+            position.EmpolyeeId = 100;
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (employeeLogic.CreateSkillMatrix(position))
+                {
+                    return View("Index");
+                    //return RedirectToAction("PositionList", "HR", new { SpecializationId = position.SpecializationId });
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
         }
     }
 }
